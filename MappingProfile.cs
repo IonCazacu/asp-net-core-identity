b8ccf2b4676056fdf5fe329e0e2000127eead400
IonCazacu <90177337+IonCazacu@users.noreply.github.com>
2025-08-24 12:20:12 +0300

    Clarify mapping comment in MappingProfile
    Updated the comment to more concisely describe mapping Email from the registration DTO to UserName in the User entity.

--- MappingProfile.cs
+++ MappingProfile.cs
@@ -8,9 +8,7 @@ public class MappingProfile : Profile
 {
     public MappingProfile()
     {
-        // When mapping from the User for registration object to the User object
-        // I want to populate the UserName property inside the User class
-        // from Email property that resides inside the User for registration class
+        // Map Email from registration DTO to UserName in User entity
         CreateMap<UserForRegistrationDto, User>()
             .ForMember(u => u.UserName, o => o.MapFrom(x => x.Email));
     }
