e4f392b6879a34f3439e18dfc630825016a36c4d
IonCazacu <90177337+IonCazacu@users.noreply.github.com>
2025-08-24 12:17:28 +0300

    Update .gitignore and clean Program.cs comments
    Added private.txt to .gitignore to exclude private files. Removed unused AutoMapper import and outdated EF migration comments from Program.cs for improved clarity.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,7 @@
-using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using IdentityUserRegistration;
 using IdentityUserRegistration.Entities;
 using Microsoft.AspNetCore.Identity;
-
-// dotnet ef migrations add initial
-// dotnet ef database update --connection "User ID=ioncazacu;Password=password;Host=localhost;Port=5432;Database=IdentityUserRegistration;Pooling=true"
-// dotnet ef database update --connection "Host=localhost;Port=5432;Database=IdentityUserRegistration;Username=ioncazacu;Pooling=true"
 
 var builder = WebApplication.CreateBuilder(args);
 
